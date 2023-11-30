955e4174e09ba43abd1c15f0ae282877967bcbbe
Smart-64 <cahimiron@yandex.ru>
2023-11-30 14:53:00 +0300

    Develop simple DB and WEB
    - Test web REST

---
+++ src/main/java/org/example/Controller/PaymentController.java
@@ -0,0 +1,46 @@
+package org.example.Controller;
+
+import org.example.Utils.PaymentRequest;
+import org.example.Utils.BaseResponse;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+    private final String sharedKey = "SHARED_KEY";
+
+    private static final String SUCCESS_STATUS = "success";
+    private static final String ERROR_STATUS = "error";
+    private static final int CODE_SUCCESS = 100;
+    private static final int AUTH_FAILURE = 102;
+
+    @GetMapping
+    public BaseResponse showStatus() {
+        return new BaseResponse(SUCCESS_STATUS, 777);
+    }
+
+    @PostMapping("/pay")
+    public BaseResponse pay(@RequestParam(value = "key") String key, @RequestBody PaymentRequest request) {
+
+        final BaseResponse response;
+
+        if (sharedKey.equalsIgnoreCase(key)) {
+            int userId = request.getUserId();
+            String itemId = request.getItemId();
+            double discount = request.getDiscount();
+            // Process the request
+            // ....
+            // Return success response to the client.
+            response = new BaseResponse(SUCCESS_STATUS, CODE_SUCCESS);
+        } else {
+            response = new BaseResponse(ERROR_STATUS, AUTH_FAILURE);
+        }
+        return response;
+    }
+}
