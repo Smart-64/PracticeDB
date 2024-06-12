2e180ee919a6df2140acbd5fb68f96cb1d71f465
Smart-64 <cahimiron@yandex.ru>
2024-06-12 11:20:30 +0300

    Velocity correct package

--- src/main/java/org/example/repository/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repository;
-
-import org.example.Utils.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Integer> {
-}
--- src/main/java/ru/velocity/Application.java
+++ src/main/java/ru/velocity/Application.java
@@ -1,4 +1,4 @@
-package org.example;
+package ru.velocity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
